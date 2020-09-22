496fb5fb9b2e33ebadbad9e46b1837f875a2d2ad
Arnab-Banerjee-Kolkata <arnabb547@gmail.com>
2020-09-22 19:48:59 +0530

    Fixes bug

--- app/src/main/java/android/arnab/organisationalstudent/StudentHome.java
+++ app/src/main/java/android/arnab/organisationalstudent/StudentHome.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -101,6 +100,7 @@ public class StudentHome extends AppCompatActivity implements View.OnClickListen
                     public void onResponse(String response)
                     {
                         JSONObject jsonResponse= null;
+                        //msg.setText(response);
                         homeWait.setVisibility(View.GONE);
                         makeWindowResponsive();
                         try {
